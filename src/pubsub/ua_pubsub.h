b99bc1e206122d0fe4f237e7028785a8507d43bf
FraserG01 <fraser@iotechsys.com>
2022-08-29 14:37:28 +0100

    XRT-1786: added back previous stack changes

--- src/pubsub/ua_pubsub.h
+++ src/pubsub/ua_pubsub.h
@@ -90,9 +90,6 @@ UA_PubSubConnectionConfig_clear(UA_PubSubConnectionConfig *connectionConfig);
 
 void
 UA_PubSubConnection_clear(UA_Server *server, UA_PubSubConnection *connection);
-/* Register channel for given connectionIdentifier */
-UA_StatusCode
-UA_PubSubConnection_regist(UA_Server *server, UA_NodeId *connectionIdentifier);
 
 /* Register channel for given connectionIdentifier */
 UA_StatusCode
